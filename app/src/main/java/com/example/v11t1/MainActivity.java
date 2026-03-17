ea44c5f86395ca269b71a8b534f8870fcd048ec1
Juho Varila <varila.juho@gmail.com>
2026-03-17 23:36:58 +0200

    Deleted some imports

--- app/src/main/java/com/example/v11t1/MainActivity.java
+++ app/src/main/java/com/example/v11t1/MainActivity.java
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 
